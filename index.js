ee6f8de2ac2a43f24584c02c0b83736421c6cf46
prince18EAYCS078 <107923819+prince18EAYCS078@users.noreply.github.com>
2022-08-29 14:12:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import Express from 'express';
+import bodyparser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
+const app = Express();
+const PORT = 5000;
+
+app.use(bodyparser.json());
+
+app.use('/users',usersRoutes);
+
+app.get('/', (req,res) => {
+     res.send('hello from home page.');
+});
+
+app.listen(PORT, () => console.log(`server running on port: http://localhost${PORT}`));
